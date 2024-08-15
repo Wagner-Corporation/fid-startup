7798be8bb020b0a092b586b3e7fb18389fd41f2b
DieterTackAu <Dieter.Tack@wagnerco.com.au>
2024-08-16 08:58:52 +1000

    Updated FID-VER-GATE2
    updated cd commands

--- FID-VER-GATE2/pull_and_start.sh
+++ FID-VER-GATE2/pull_and_start.sh
@@ -2,10 +2,10 @@
 sleep 10s
 
 # Navigate to the directory containing the repository
-cd /path/to/repository
+cd /fid-startup
 
 # Pull the latest version of the repository
 git pull origin main
 
 # Execute the updated startup script
-bash /fid-startup/FID-VER-GATE2/startup-script.sh
+bash /fid-startup/FID-VER-GATE2/start_chrome.sh
