68e8010e03ad61ab5d24fa8c27267580da0a0523
Jamie Barron <70959875+barronjamie@users.noreply.github.com>
2024-08-15 12:40:56 +1000

    Adding files.

---
+++ FID-HOR-CHCK05/pull_and_start.sh
@@ -0,0 +1,8 @@
+# Navigate to the directory containing the repository
+cd /path/to/repository
+
+# Pull the latest version of the repository
+git pull origin main
+
+# Execute the updated startup script
+bash /path/to/startup-script.sh
