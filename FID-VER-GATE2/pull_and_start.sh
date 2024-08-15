5fbb1140ba523b3988aea824b20afef36f210327
Jamie Barron <70959875+barronjamie@users.noreply.github.com>
2024-08-16 09:50:30 +1000

    Sleep update

--- FID-VER-GATE2/pull_and_start.sh
+++ FID-VER-GATE2/pull_and_start.sh
@@ -1,4 +1,4 @@
-#Sleep for 10 Seconds
+# Sleep 5s
 sleep 10s
 
 # Navigate to the directory containing the repository
@@ -6,6 +6,9 @@ cd /home/fid/fid-startup
 
 # Pull the latest version of the repository
 git pull origin main
+
+# Sleep 5s
+sleep 5s
 
 # Execute the updated startup script
 google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk https://fid.wagnerco.com.au --password-store=basic
