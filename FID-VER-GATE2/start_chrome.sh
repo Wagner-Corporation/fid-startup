952e7645c43f19c4b8c84006eec8e5788df70145
Jamie Barron <70959875+barronjamie@users.noreply.github.com>
2024-08-16 10:52:49 +1000

    Update start_chrome.sh

--- FID-VER-GATE2/start_chrome.sh
+++ FID-VER-GATE2/start_chrome.sh
@@ -1,2 +1,2 @@
 # Start chrome in kiosk mode, with a veriable to use the basic password store
-google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk https://fid.wagnerco.com.au/event.html --password-store=basic
+google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk https://fid.wagnerco.com.au/FID-Portrait.html --password-store=basic
