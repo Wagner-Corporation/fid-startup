b597ac2ea18d23e6c144849c918de6123b5f7876
Jamie Barron <70959875+barronjamie@users.noreply.github.com>
2024-08-16 10:14:22 +1000

    testing cache urls

--- FID-VER-GATE2/pull_and_start.sh
+++ FID-VER-GATE2/pull_and_start.sh
@@ -20,4 +20,4 @@ sleep 5s
 rm -rf ~/.config/google-chrome/Default/Cache/*
 
 # Execute the updated startup script
-google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk https://fid.wagnerco.com.au/FID-Portrait.html --password-store=basic
+google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk https://google.com.au --password-store=basic
