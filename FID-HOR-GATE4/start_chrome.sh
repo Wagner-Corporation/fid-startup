9029cca4e5d6d2f0e787a327ebd042d5bb5181a3
Jamie Barron <70959875+barronjamie@users.noreply.github.com>
2024-08-16 11:20:49 +1000

    Gate Screens HOR

--- FID-HOR-GATE4/start_chrome.sh
+++ FID-HOR-GATE4/start_chrome.sh
@@ -1,2 +1,2 @@
-# Wait for 10 seconds then start chrome in kiosk mode, with a veriable to use the basic password store
-sleep 10s && google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk https://fids.wagnerco.com.au/gate-4.html --password-store=basic
+# Start chrome in kiosk mode, with a veriable to use the basic password store
+google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk https://fids.wagnerco.com.au/gate-4.html --password-store=basic
