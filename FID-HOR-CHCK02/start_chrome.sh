359cfbc5009cd11094c17bfba871724d8cc16d34
Jamie Barron <70959875+barronjamie@users.noreply.github.com>
2024-08-16 11:18:44 +1000

    Check In Screens

--- FID-HOR-CHCK02/start_chrome.sh
+++ FID-HOR-CHCK02/start_chrome.sh
@@ -1,2 +1,2 @@
-# Wait for 10 seconds then start chrome in kiosk mode, with a veriable to use the basic password store
-sleep 10s && google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk /home/fid/web/CheckIn4.html --password-store=basic
+# Start chrome in kiosk mode, with a veriable to use the basic password store
+google-chrome --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' --kiosk https://fids.wagnerco.com.au/check-in-2.html --password-store=basic
